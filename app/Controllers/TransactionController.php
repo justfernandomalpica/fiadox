1a13b0d873ebfd55a9c28b69c63dd2a0b3742fed
Fernando De Los Santos Malpica <fernando_santosmalpica@outlook.com>
2026-04-28 18:17:20 -0600

    (feat) Added endpoint to fetch transaction grand total.

--- app/Controllers/TransactionController.php
+++ app/Controllers/TransactionController.php
@@ -24,6 +24,34 @@ class TransactionController {
         Response::success(["La Transacción se guardó correctamente.", $transaction],201);
     }
 
+    public static function getBalance() {
+        $result = TransactionModel::getBalance();
+        Response::success($result);
+    }
+
+    public static function filterById(array $props) {
+        if(!isset($props["id"])) Response::error("No se encontró Id de cliente en la petición");
+        if(!isset($_GET["page"]) || !isset($_GET["per_page"])) Response::error("Los campos de paginación son obligatorios");
+
+        $result = TransactionModel::where("client_id", $props["id"], $_GET["page"], $_GET["per_page"]);
+        if(empty($result)) Response::error("El cliente no tiene transacciones o no existe", 404);
+        
+        $transactions = array_map(function ($row) {
+            return [
+                'id' => $row->id,
+                'amount'=> $row->getAmount(),
+                'type'  => $row->type,
+                'time'  => $row->created_at
+            ];
+        }, $result);
+        
+        Response::success([
+            "page"=>$_GET["page"],
+            "per_page"=>$_GET["per_page"],
+            "transactions"=>$transactions
+        ]);
+    }
+
     private static function validate(){
         if( !isset($_POST["client_id"]) ||
             !isset($_POST["type"]) ||
